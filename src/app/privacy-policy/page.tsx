c74cb10a706f8fa9e20f3b0b400bcd7eb7ff386b
greatSumini <greatSumini@gmail.com>
2025-03-06 03:12:21 +0900

    feat: 메타데이터 설정

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,4 +1,10 @@
 import { PrivacyPolicyPage } from "@/features/privacy-policy/page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "개인정보처리방침 작성기",
+  description: "개인정보처리방침을 단계별로 작성하고 생성합니다.",
+};
 
 export default function Page() {
   return <PrivacyPolicyPage />;
